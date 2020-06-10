e4bd9ce3626392ef09019a7c1686381dd83e11aa
Israe Calderón <icalderond@outlook.com>
2020-06-10 15:46:59 -0500

    Agregando Behaviors para solo letras

---
+++ MarcaApp/Behaviors/TextValidationBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Xamarin.Forms;
+
+namespace MarcaApp.Behaviors
+{
+    public class TextValidationBehavior : Behavior<Entry>
+    {
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            bindable.TextChanged += OnEntryTextChanged;
+            base.OnAttachedTo(bindable);
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            bindable.TextChanged -= OnEntryTextChanged;
+            base.OnDetachingFrom(bindable);
+        }
+
+        private void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (Regex.IsMatch(e.NewTextValue, @"^[a-zA-Z]+$"))
+            {
+                ((Entry)sender).TextColor = Color.Black;
+            }
+            else
+            {
+                ((Entry)sender).TextColor = Color.Red;
+            }
+        }
+    }
+}
